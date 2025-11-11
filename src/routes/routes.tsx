8a8f312fb6128e849b327731be552b2153643728
DaniloAlvesantos <daniloasan.itapira@gmail.com>
2025-11-11 15:16:03 -0300

    remove: upload page

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import { App } from "../App";
 import { MapPage } from "../pages/map";
-import { Upload } from "../pages/upload";
 import { LoginPage } from "../pages/login";
 import { SignUpPage } from "../pages/signUp";
 
@@ -10,7 +9,6 @@ export const AppRoutes = () => {
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/map" element={<MapPage />} />
-      <Route path="/upload" element={<Upload />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignUpPage />} />
       {/* Catch-all 404 route */}
